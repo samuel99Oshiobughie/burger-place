eedf1a6a159d179a0d103884b5f69ad6b382f0b1
samuel <samuel@samuels-MacBook-Pro.local>
2023-07-26 20:04:33 +0100

    add new files

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       res.status(200).json(products);
     } catch (err) {
       res.status(500).json(err);
+      console.log('err: ', err)
     }
   }
 
